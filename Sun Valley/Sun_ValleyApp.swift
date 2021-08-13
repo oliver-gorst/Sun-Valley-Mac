e3a1394c15a5ffa75b656a41c8da272c0dcda794
Oliver Gorst <oliver.gorst@outlook.com>
2021-08-13 14:39:31 -0400

    Initial Commit

---
+++ Sun Valley/Sun_ValleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sun_ValleyApp.swift
+//  Sun Valley
+//
+//  Created by Oliver Gorst on 8/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Sun_ValleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
