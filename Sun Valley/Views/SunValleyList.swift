5e22151a95f520a7b64fdb19c96ebac8a67962b6
Oliver Gorst <oliver.gorst@outlook.com>
2021-09-24 22:30:41 -0400

    adding SunValleyList, with a navigation section, and navigation title

---
+++ Sun Valley/Views/SunValleyList.swift
@@ -0,0 +1,23 @@
+//
+//  SunValleyList.swift
+//  Sun Valley
+//
+//  Created by Oliver Gorst on 9/24/21.
+//
+
+import SwiftUI
+
+struct SunValleyList: View {
+    var body: some View {
+        NavigationView {
+            Text("Navigation Area")
+            .navigationTitle("Nav Title")
+        }
+    }
+}
+
+struct SunValleyList_Previews: PreviewProvider {
+    static var previews: some View {
+        SunValleyList()
+    }
+}
