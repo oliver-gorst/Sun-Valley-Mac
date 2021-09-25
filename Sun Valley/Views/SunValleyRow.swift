600aa9789f2f2dcf5693d7459b73ed76f658dc5e
Oliver Gorst <oliver.gorst@outlook.com>
2021-09-24 22:03:50 -0400

    initial creation of SunValleyRow.swift view,

---
+++ Sun Valley/Views/SunValleyRow.swift
@@ -0,0 +1,23 @@
+//
+//  SunValleyRow.swift
+//  Sun Valley
+//
+//  Created by Oliver Gorst on 9/24/21.
+//
+
+import SwiftUI
+
+struct SunValleyRow: View {
+    var body: some View {
+        HStack {
+            Text("Meow Meow")
+            Text("Purr Purr")
+        }
+    }
+}
+
+struct SunValleyRow_Previews: PreviewProvider {
+    static var previews: some View {
+        SunValleyRow()
+    }
+}
