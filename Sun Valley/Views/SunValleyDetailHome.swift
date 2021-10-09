02c86f0ad35d9be177320209df71f2fd918cf189
Oliver Gorst <oliver.gorst@outlook.com>
2021-10-09 00:57:21 -0400

    Changed the initial view stack to SunValleyDetail4

---
+++ Sun Valley/Views/SunValleyDetailHome.swift
@@ -0,0 +1,21 @@
+//
+//  SunValleyDetail.swift
+//  Sun Valley
+//
+//  Created by Oliver Gorst on 9/24/21.
+//
+
+import SwiftUI
+
+struct SunValleyDetailHome: View {
+    var body: some View {
+        Text("Trail Map Detail Home")
+            .foregroundColor(.red)
+    }
+}
+
+struct SunValleyDetailHome_Previews: PreviewProvider {
+    static var previews: some View {
+        SunValleyDetailHome()
+    }
+}
