5b5de2106b0f28bc05a9f11b6b5f8e366fad3cbe
Oliver Gorst <oliver.gorst@outlook.com>
2021-11-07 23:24:27 -0500

    addition of historical trail maps to the control selector

--- Sun Valley/Views/HomeDetail.swift
+++ Sun Valley/Views/HomeDetail.swift
@@ -1,19 +1,22 @@
 import SwiftUI
 
 struct HomeDetail: View {
-    @State private var favoriteColor = "1964"
-    var colors = ["1964", "1982", "2002"]
+    @State private var mapYear = "1945"
+    var years = ["1945", "1959", "1985", "1995", "2021"]
 
     var body: some View {
         VStack {
-            Picker("What is your favorite color?", selection: $favoriteColor) {
-                ForEach(colors, id: \.self) {
+            Picker("What is your favorite color?", selection: $mapYear) {
+                ForEach(years, id: \.self) {
                     Text($0)
                 }
             }
             .pickerStyle(.segmented)
 
-            Text("Value: \(favoriteColor)")
+            Text("The Trail Map for: \(mapYear)")
+            Image(mapYear)
+                .resizable()
+                .aspectRatio(contentMode:.fit)
             Spacer()
         }
     }
