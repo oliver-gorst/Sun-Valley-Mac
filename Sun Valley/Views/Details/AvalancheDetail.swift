267097d337c1540c6e9f878fd49747df9f515d7a
Oliver Gorst <oliver.gorst@outlook.com>
2021-11-19 14:56:19 -0500

    Addition of switch for avalanche level, addition of new images for avalanche level. Next step is to be able to call out image, and set background color based on switch output

--- Sun Valley/Views/Details/AvalancheDetail.swift
+++ Sun Valley/Views/Details/AvalancheDetail.swift
@@ -7,9 +7,38 @@
 
 import SwiftUI
 
+
+
 struct AvalancheDetail: View {
+    @State var warningImage = ""
+    func avalancheRandom() -> String {
+        let avalancheNumber = Int.random(in: 1...4)
+        switch avalancheNumber{
+        case 1:
+            warningImage = "\"Low\""
+            return("The avalanche rating is 1")
+        case 2:
+            warningImage = "\"Moderate\""
+            return("The avalanche rating is 2")
+        case 3:
+            warningImage = "\"Considerable\""
+            return("The avalanche rating is 3")
+        case 4:
+            warningImage = "\"High\""
+            return("The avalanche rating is 4")
+        default:
+            return("meow")
+        }
+    }
+    
     var body: some View {
+        VStack{
+        Text(avalancheRandom())
+            Image(warningImage)
+                .resizable()
+                .aspectRatio(contentMode:.fit)
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
